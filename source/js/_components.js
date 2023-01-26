16a363dab7984f739e17badff7e03e5254d92ea2
roman <romasaab@gmail.com>
2023-01-26 23:23:42 +0200

    add project

---
+++ source/js/_components.js
@@ -0,0 +1,12 @@
+// import './components/accordions';
+// import './components/anchor';
+// import './components/lift';
+// import './components/galery';
+// import './components/modals';
+// import './components/sliders';
+// import './components/replaceEL';
+// import './components/getDinamicHeight';
+// import './components/lightbox';
+// import './components/select';
+// import './components/tabs';
+
