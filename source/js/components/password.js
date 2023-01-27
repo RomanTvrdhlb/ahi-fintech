197f588aa6b5fa7f74d84d51545f0afea1029b2c
roman <romasaab@gmail.com>
2023-01-27 11:05:09 +0200

    fix js

--- source/js/components/password.js
+++ source/js/components/password.js
@@ -1,6 +1,15 @@
 const pass = prompt('输入密码');
-if (pass=='72127387348') alert('密碼正確');
+
+if (pass=='72127387348');
 else { 
     alert('密碼錯誤!'); 
-    top.location.href=""; 
+    AccessDenied();
+    top.location.href="";
 }
+
+function AccessDenied() {
+    document.body.innerHTML = '訪問被拒絕!';
+    document.body.style.cssText = `
+     opacity: 0;
+    `;
+  }
