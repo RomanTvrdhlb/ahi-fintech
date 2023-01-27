5389f33ce2c0d399f4c8185ca6103922cad59932
roman <romasaab@gmail.com>
2023-01-27 11:25:36 +0200

    fix js

--- source/js/components/password.js
+++ source/js/components/password.js
@@ -5,22 +5,15 @@ import {
 import vars from "../_vars";
 
 const { bodyEl } = vars;
-
 const pass = prompt("输入密码");
 
-AccessDenied();
 if (pass === "72127387348") {
-  AccessСonfirmed();
+    AccessСonfirmed();
 } else {
   alert("密碼錯誤!");
-  AccessDenied();
   top.location.href = "";
 }
 
-function AccessDenied() {
-  addCustomClass(bodyEl, "active");
-}
-
 function AccessСonfirmed() {
-  removeCustomClass(bodyEl, "active");
+  removeCustomClass(bodyEl, "alert");
 }
